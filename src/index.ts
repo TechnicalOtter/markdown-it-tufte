e33e666287906624b0c4bd1e491cfe69c53d6d26
Neill Robson <neill.robson@pendo.io>
2025-01-08 08:56:26 -0500

    begin sandbox build from scratch

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import type MarkdownIt from "markdown-it/lib"
 import newthought_plugin from "./rules/newthought"
-import sidenote_plugin from "./rules/sidenote"
+import sidenote_plugin from "./rules/sandbox"
 
 export default function plugin(md: MarkdownIt) {
   newthought_plugin(md)
---
+++ src/rules/sandbox.ts
@@ -0,0 +1,108 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import MarkdownIt, { Options } from "markdown-it"
+import Renderer from "markdown-it/lib/renderer"
+import StateBlock from "markdown-it/lib/rules_block/state_block"
+import Token from "markdown-it/lib/token"
+
+function render_footnote_def(
+  tokens: Token[],
+  idx: number,
+  options: Options,
+  env: any,
+  slf: Renderer
+) {
+  return `<aside>footnote</aside>`
+}
+
+export default function footnote_plugin(md: MarkdownIt) {
+  md.renderer.rules.footnote_def = render_footnote_def
+
+  // Process footnote block definition
+  function footnote_def(
+    state: StateBlock,
+    startLine: number,
+    endLine: number,
+    silent: boolean
+  ) {
+    // ################
+    // ### IDENTIFY ###
+    // ################
+
+    const start = state.bMarks[startLine] + state.tShift[startLine]
+    const max = state.eMarks[startLine]
+
+    // line should be at least 5 chars - "[^x]:"
+    if (start + 4 > max) return false
+
+    if (state.src.charCodeAt(start) !== 0x5b /* [ */) return false
+    if (state.src.charCodeAt(start + 1) !== 0x5e /* ^ */) return false
+
+    let pos
+
+    for (pos = start + 2; pos < max; pos++) {
+      if (state.src.charCodeAt(pos) === 0x20) return false
+      if (state.src.charCodeAt(pos) === 0x5d /* ] */) {
+        break
+      }
+    }
+
+    if (pos === start + 2) return false // no empty footnote labels
+    if (pos + 1 >= max || state.src.charCodeAt(++pos) !== 0x3a /* : */) return false
+    if (silent) return true
+    pos++
+
+    // #############
+    // ### PARSE ###
+    // #############
+    // Set the indent to "inside" the footnote, and tokenize subsequent blocks
+    // that fall within that indent.
+
+    const oldBMark = state.bMarks[startLine]
+    const oldTShift = state.tShift[startLine]
+    const oldSCount = state.sCount[startLine]
+
+    const posAfterColon = pos
+    const initial =
+      state.sCount[startLine] +
+      pos -
+      (state.bMarks[startLine] + state.tShift[startLine])
+    let offset = initial
+
+    while (pos < max) {
+      const ch = state.src.charCodeAt(pos)
+
+      if (md.utils.isSpace(ch)) {
+        if (ch === 0x09) {
+          offset += 4 - (offset % 4)
+        } else {
+          offset++
+        }
+      } else {
+        break
+      }
+
+      pos++
+    }
+
+    state.tShift[startLine] = pos - posAfterColon
+    state.sCount[startLine] = offset - initial
+
+    state.bMarks[startLine] = posAfterColon
+    state.blkIndent += 4
+
+    if (state.sCount[startLine] < state.blkIndent) {
+      state.sCount[startLine] += state.blkIndent
+    }
+
+    state.md.block.tokenize(state, startLine, endLine)
+
+    state.blkIndent -= 4
+    state.tShift[startLine] = oldTShift
+    state.sCount[startLine] = oldSCount
+    state.bMarks[startLine] = oldBMark
+
+    return true
+  }
+
+  md.block.ruler.before("reference", "footnote_def", footnote_def)
+}
